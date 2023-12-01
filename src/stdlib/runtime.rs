501ec11a69e168ee29de8bc931077f753a28ef73
vcokltfre <vcokltfre@gmail.com>
2023-12-01 00:07:51 +0000

    feat(stdlib): add runtime module

---
+++ src/stdlib/runtime.rs
@@ -0,0 +1,28 @@
+use crate::vm::{VMValue, VM};
+
+pub fn runtime_goto(
+    vm: &mut VM,
+    _idts: Vec<Option<String>>,
+    args: Vec<VMValue>,
+) -> Result<Option<VMValue>, String> {
+    if args.len() != 1 {
+        return Err(format!("expected 1 argument, got {}", args.len()));
+    }
+
+    let label = match args[0].clone() {
+        VMValue::String(label) => label,
+        _ => return Err(format!("expected string, got {}", args[0].name())),
+    };
+
+    match vm.gotos.get(&label) {
+        Some(index) => {
+            vm.index = *index;
+            Ok(None)
+        }
+        None => Err(format!("label {} not found", label)),
+    }
+}
+
+pub fn register(vm: &mut VM) {
+    vm.register("runtime".to_string(), "gotolabel".to_string(), runtime_goto);
+}
